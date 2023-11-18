f3fb4ac02abfe2e356efcf5899e639bd79f1714c
George Nijo <nijog@wit.edu>
2023-11-18 02:39:52 -0500

    New Screen File
    Separated the Screens into their own files for ease of management and development

---
+++ dashboard_screen.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
+
+class DashboardScreen(QWidget):
+    def __init__(self, stacked_widget):
+        super().__init__()
+        self.stacked_widget = stacked_widget
+        self.initUI()
+
+    def initUI(self):
+        self.dashboard_label = QLabel('Dashboard - Main app functionality', self)
+        self.settings_button = QPushButton('Settings', self)
+        self.settings_button.clicked.connect(self.gotoSettings)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.dashboard_label)
+        layout.addWidget(self.settings_button)
+
+        self.setLayout(layout)
+        self.setWindowTitle('Dashboard')
+
+    def gotoSettings(self):
+        self.stacked_widget.setCurrentIndex(2)
